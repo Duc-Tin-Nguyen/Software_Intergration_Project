71b1528413aa2c6d861fd7e18aa9397105cb9b2f
Duc-Tin-Nguyen <tinnd1506@gmail,com>
2024-08-02 13:29:42 +0700

    refactor: migrate to Typescript

---
+++ src/routes/messages.routes.ts
@@ -0,0 +1,18 @@
+import express, { Router } from 'express';
+import {
+  addMessage,
+  getMessages,
+  editMessage,
+  deleteMessage,
+  getMessageById
+} from '../controllers/messages.controller';
+
+const router: Router = express.Router();
+
+router.post('/add/message', addMessage);
+router.get('/', getMessages);
+router.put('/edit/:messageId', editMessage);
+router.delete('/delete/:messageId', deleteMessage);
+router.get('/:messageId', getMessageById);
+
+export default router;
